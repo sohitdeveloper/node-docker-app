fbeecaa52dc78c71f45bdc1a04d7a2a03e6e4ac7
Sohit <sohit@digitalgreen.org>
2023-08-11 12:00:31 +0530

    added delay for testing purpose.

--- app.js
+++ app.js
@@ -5,7 +5,10 @@ const app = express();
 const port = process.env.PORT || 8000;
 app.get("/", (req, res) => {
   console.log(process.env.ENV_NAME);
-  res.send("Voila! you are in " + process.env.ENV_NAME);
+
+  setTimeout(() => {
+    res.send("Voila! you are in " + process.env.ENV_NAME);
+  }, 5000);
 });
 
 app.listen(port, () => {
